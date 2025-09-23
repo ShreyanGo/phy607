ee93ce1735b92bef24297cf320c5b0177c6ac712
aabruscato <arabrusc@syr.edu>
2025-09-23 11:21:03 -0400

    changed names of functions to be active verbs. fixed logical issue in the Runge Kutta function to store updated PE & KE values as the system is changing.

--- rungekutta.py
+++ rungekutta.py
@@ -2,22 +2,22 @@
 # Runge Kutta
 import numpy as np
 
-def next_value(y, t, step=0.1, mass=10, k=1): ### needs better name
-    k1 = func(y, t, mass, k)
-    k2 = k_2(y, t, step, k1, mass, k)
-    return y_next(y, k1, k2, step)
+def calc_next_step(y, t, step=0.1, mass=10, k=1):
+    k1 = compute_derivaties(y, t, mass, k)
+    k2 = compute_k2(y, t, step, k1, mass, k)
+    return update_y(y, k1, k2, step)
 
-def func(y, t, mass, k):
-    f = [y[1]] # velocity, dy_0/dt
-    f.append(-k*y[0]/mass) # acceleration, dy_1/dt ###fix
-    return f
+def compute_derivaties(y, t, mass, k):
+    derivative = [y[1]] # velocity, dy_0/dt
+    derivative.append(-k*y[0]/mass) # acceleration, dy_1/dt
+    return derivative
 
-def k_2(y, t, step, k_1, mass, k):
+def compute_k2(y, t, step, k_1, mass, k):
     yaux = [y[0]+step * k_1[0]/2]
     yaux.append(y[1]+step * k_1[1]/2)
-    return func(yaux, t+step/2, mass, k)
+    return compute_derivaties(yaux, t+step/2, mass, k)
 
-def y_next(y, k_1, k_2, step):
+def update_y(y, k_1, k_2, step):
     y[0] = y[0] + step*(k_1[0] + 2*k_2[0])/6 # new position
     y[1] = y[1] + step*(k_1[1] + 2*k_2[1])/6 # new velocity
     return y
@@ -40,12 +40,14 @@ def RungeKutta(pos_0, vel_0, step_size=0.1, max_time=10, mass=10, k=1):
     #total_energy = [KE_arr[0] + PE_arr[0]]
     total_energy = []
     for time in time_arr:
-        y = next_value(y, time)
+        y = calc_next_step(y, time)
         pos_arr.append(y[0])
         vel_arr.append(y[1])
-        PE = -k * x_0**2
+        pos = pos_arr[-1]
+        vel = vel_arr[-1]
+        PE = -k * pos**2
         PE_arr.append(PE)
-        KE = 0.5 * mass * vel_0**2
+        KE = 0.5 * mass * vel**2
         KE_arr.append(KE)
         total_energy.append(PE + KE)
 
